1011214768e550e8b82d582c22412c4d1ba720de
socilogue1 <sociedadepelosangue@gmail.com>
2023-08-27 18:42:19 -0300

    ajustes

--- src/maintenance/maintenance.module.ts
+++ src/maintenance/maintenance.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MaintenanceService } from './maintenance.service';
 import { MaintenanceController } from './maintenance.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { MaintenanceEntity } from './entities/maintenance.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([MaintenanceEntity])],
   controllers: [MaintenanceController],
   providers: [MaintenanceService],
+  exports: [MaintenanceService],
 })
 export class MaintenanceModule {}
