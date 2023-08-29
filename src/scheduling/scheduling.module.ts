b650f94ca4873b5649674cfac88d1cc2f5c06bdc
socilogue1 <sociedadepelosangue@gmail.com>
2023-08-29 14:07:36 -0300

    Ajustes

--- src/scheduling/scheduling.module.ts
+++ src/scheduling/scheduling.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { SchedulingService } from './scheduling.service';
 import { SchedulingController } from './scheduling.controller';
+import { SchedulingEntity } from './entities/scheduling.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([SchedulingEntity])],
   controllers: [SchedulingController],
   providers: [SchedulingService],
+  exports: [SchedulingService],
 })
 export class SchedulingModule {}
