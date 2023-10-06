dafab4c900d0ef8971909a3e8d608d491c15bccb
socilogue1 <sociedadepelosangue@gmail.com>
2023-10-06 00:41:37 -0300

    Ajustes

--- src/migration/1693167293413-insert_table_instrument.ts
+++ src/migration/1693167293413-insert_table_instrument.ts
@@ -8,7 +8,11 @@ export class InsertTableInstrument1693167293413 implements MigrationInterface {
         ('Furadeira', 'Ferramenta para perfuração de furos', 'Instrument', 1),
         ('Serra Elétrica', 'Ferramenta para corte de materiais', 'Instrument', 2),
         ('Chave de Fenda', 'Ferramenta para apertar parafusos', 'Instrument', 3),
-        ('Martelo', 'Ferramenta para bater em objetos', 'Instrument', 4);
+        ('Martelo', 'Ferramenta para bater em objetos', 'Instrument', 4),
+        ('Martelo', 'Ferramenta para bater em objetos', 'Instrument', 1),
+        ('Serra Circular', 'Ferramenta para corte circular de materias', 'Instrument', 2)
+        ('Trator', 'Veículo', 'Machine', 1),
+        ('Trator', 'Veículo', 'Machine', 2);
     `);
   }
 
