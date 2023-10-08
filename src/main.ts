ce5392f57ec4265f7c99348ffcccb7f208042b2c
socilogue1 <sociedadepelosangue@gmail.com>
2023-10-08 02:58:53 -0300

    Ajustes

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
       'Origin, X-Requested-With, Content-Type, Accept',
     );
 
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH'); // Correção aqui
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-PATHS', '*');
 
